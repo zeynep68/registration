d02d45cdb4be717a03694df1d8eb72e216cdbe06
zeynep68 <zeynep.boztoprak@hhu.de>
2024-06-26 00:15:46 +0200

    init

--- utils2.py
+++ utils2.py
@@ -19,12 +19,22 @@ def to_itk(array, spacing, dtype=np.float32):
     return image
 
 
-def load_images(bf_pyramid_level, trans_pyramid_level):
+def process_string(section_no):
+	n = len(section_no)
+	
+	out = "0" * n
+	out += section_no
+	return out
+
+
+def load_images(bf_pyramid_level, trans_pyramid_level, section):
 	# load two images : one blockface + one transmittance
 	data = json.load(open('pli_paths.json'))
+	out = process_string(section)
+	print(section, out)
 
-	bf_path = data['blockface']['348']
-	trans_path = data['transmittance']['348']
+	bf_path = data['blockface'][section]
+	trans_path = data['transmittance'][section]
 
 	bf_path = bf_path.replace("/p/data1/", "/home/zeynepboztoprak/p/data1/")
 	trans_path = trans_path.replace("/p/data1/", "/home/zeynepboztoprak/p/data1/")
@@ -32,17 +42,20 @@ def load_images(bf_pyramid_level, trans_pyramid_level):
 	bf_fd = h5py.File(bf_path, 'r')
 	trans_fd = h5py.File(trans_path, 'r')
 
-	trans_mask_path = "/p/data1/pli/DB_data/PE-2021-00981-H_ImageData/Brain_Part_00/Section_0348/PM/Complete/Mask/PE-2021-00981-H_00_s0348_PM_Complete_Mask_Stitched_Flat_v009.h5"
+	trans_mask_path = f"/p/data1/pli/DB_data/PE-2021-00981-H_ImageData/Brain_Part_00/Section_{out}/PM/Complete/Mask/PE-2021-00981-H_00_s{out}_PM_Complete_Mask_Stitched_Flat_v000.h5"
 	trans_mask_path = trans_mask_path.replace("/p/data1/", "/home/zeynepboztoprak/p/data1/")
 	trans_mask_fd = h5py.File(trans_mask_path, 'r')
 
-	bf_mask_path = "/p/data1/pli/DB_data/PE-2021-00981-H_ImageData/Brain_Part_00/Section_0348/BF/Mask/PE-2021-00981-H_00_s0348_BF_Mask_Registered_Flat_v000.h5"
+	bf_mask_path = f"/p/data1/pli/DB_data/PE-2021-00981-H_ImageData/Brain_Part_00/Section_{out}/BF/Mask/PE-2021-00981-H_00_s{out}_BF_Mask_Registered_Flat_v000.h5"
 	bf_mask_path = bf_mask_path.replace("/p/data1/", "/home/zeynepboztoprak/p/data1/")
 	bf_mask_fd = h5py.File(bf_mask_path, 'r')
 
 	if bf_pyramid_level is None:
 		bf_img = bf_fd['Image'][:]
 		bf_mask_img = bf_mask_fd['Image'][:]
+	else:
+		bf_img = bf_fd['pyramid'][bf_pyramid_level][:]
+		bf_mask_img = bf_mask_fd['pyramid'][bf_pyramid_level][:]
 
 	trans_img = trans_fd['pyramid'][trans_pyramid_level][:]
 	trans_mask_img = trans_mask_fd['pyramid'][trans_pyramid_level][:]
